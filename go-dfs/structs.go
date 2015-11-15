7547867cd0e2f3a68f4efed4325aa45e14f0bdce
sjoshi6 <sjoshi6@ncsu.edu>
2015-11-15 17:48:05 -0500

    Changed struct param

--- go-dfs/structs.go
+++ go-dfs/structs.go
@@ -3,6 +3,6 @@ package main
 // ReverseIndex : Used to store Reverse Index information in Redis
 // Useful for marshal and unmarshalling
 type ReverseIndex struct {
-	PrefixPath  string
-	Destination string
+	AbsolutePath string
+	Destination  string
 }
